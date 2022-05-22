88652c90caaa9e8b41270d839a045cef9efa9c88
Mark McDermott <mark@markmcdermott.io>
2022-05-22 12:25:59 -0500

    adding leaderboard to automatically authorized pages

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,5 +1,7 @@
 class ScoresController < ApplicationController
+
   before_action :set_score, only: %i[ show edit update destroy ]
+  skip_before_action :authorized, only: [:index]
 
   # GET /scores or /scores.json
   def index
